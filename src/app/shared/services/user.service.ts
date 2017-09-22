815167c3e9cc0e654d50807f4d62358529c84323
Satawe-no <Satawe-no@users.noreply.github.com>
2017-09-22 13:46:26 +0200

    Add files via upload

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -5,6 +5,7 @@ import { Http, Response } from '@angular/http';
 // Observable
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/share';
 
 // Models
 import { IUser } from '../models/user';
@@ -22,5 +23,6 @@ export class UserService {
     getUsers(): Observable<IUser[]> {
         return this.http.get('http://www.json-generator.com/api/json/get/cpZZLDMuuW?indent=2', commonHttpHeaders)
             .map((res: Response) => res.json() as IUser[])
+            .share()
     }
 }
